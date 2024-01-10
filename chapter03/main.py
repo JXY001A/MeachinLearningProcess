382d7c8423eb64d5d9b26525fd4c5c829558be1c
jxy001a <jxy001a@aliyun.com>
2024-01-10 09:26:22 +0800

    feat: add draw3d

---
+++ chapter03/main.py
@@ -0,0 +1,9 @@
+
+from draw3dUtils import draw3d,Points3D,Arrow3D,Segment3D;
+
+draw3d(
+    Points3D((2,2,2),(1,-2,-2)),
+    Arrow3D((2,2,2)),
+    Arrow3D((1,-2,-2)),
+    Segment3D((2,2,2), (1,-2,-2))
+)
