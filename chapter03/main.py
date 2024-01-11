9f2578d5805f1ebf5166d2bd6f2113913d5bac9f
jxy001a <jxy001a@aliyun.com>
2024-01-11 23:50:38 +0800

    feat: try Box3D

--- chapter03/main.py
+++ chapter03/main.py
@@ -1,9 +1,11 @@
 
-from draw3dUtils import draw3d,Points3D,Arrow3D,Segment3D;
+from draw3dUtils import draw3d,Points3D,Arrow3D,Segment3D,Box3D;
 
 draw3d(
     Points3D((2,2,2),(1,-2,-2)),
     Arrow3D((2,2,2)),
     Arrow3D((1,-2,-2)),
-    Segment3D((2,2,2), (1,-2,-2))
+    Segment3D((2,2,2), (1,-2,-2)),
+    Box3D(2,2,2),
+    Box3D(1,-2,-2),
 )
