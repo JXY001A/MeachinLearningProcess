bbde8995ee3e68681318627c9f48f1325442d374
jxy001a <jxy001a@aliyun.com>
2024-01-12 23:50:39 +0800

    feat: test

--- chapter03/angle_between.py
+++ chapter03/angle_between.py
@@ -3,12 +3,11 @@ from math import acos,pi;
 from dot import dot;
 
 def cos_deg(v1,v2):
-  return dot(v1,v2)/length(v1)*length(v2);
+  return dot(v1,v2)/(length(v1)*length(v2));
 
 def angle_between(v1,v2):
   return acos(cos_deg(v1,v2));
 
-print(cos_deg((1,0),(0,2)))
-print(cos_deg((1,0),(2,0)))
-# print()
-# angle_between((1,0),(0,1))
+
+# 约等于：1.911
+print(angle_between((1,1,1),(-1,-1,1)));
