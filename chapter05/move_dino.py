c456495583afdad92c6b969a5e67dc0a2a8341b9
jxy001a <jxy001a@aliyun.com>
2024-01-17 09:11:56 +0800

    feat: add matrix_multiply

--- chapter05/move_dino.py
+++ chapter05/move_dino.py
@@ -2,14 +2,16 @@ import sys;
 sys.path.append('/Users/jxy001a/Code/MeachinLearningProcess')
 
 from chapter03.draw3dUtils import *;
-from matrix_multiply import matrix_multiply ;
+# from matrix_multiply import matrix_multiply ;
+from chapter04.AppendixC.vectors import multiply_matrix_vector;
+
 
 
 def dot(u,v):
   return sum(coord1* coord2 for coord1,coord2 in zip(u,v));
 
-def multiply_matrix_vector(a,b):
-  return tuple(dot(b,col) for col in zip(*a))
+# def multiply_matrix_vector(a,b):
+#   return tuple(dot(b,col) for col in zip(*a))
 
 dino_vectors = [(6,4), (3,1), (1,2), (-1,5), (-2,5), (-3,4), (-4,4),
     (-5,3), (-5,2), (-2,2), (-5,1), (-4,0), (-2,1), (-1,0), (0,-3),
@@ -30,12 +32,17 @@ magic_matrix = (
 
 translated_v = [multiply_matrix_vector(magic_matrix,v) for v in dino_3d_vectors]
 
-# draw3d(Points3D(*translated_v,color="blue"),*polygon_segment_3d(translated_v));
-# draw3d(Points3D(*dino_3d_vectors,color="blue"),*polygon_segment_3d(dino_3d_vectors));
+# draw3d(
+#   Points3D(*translated_v,color="blue"),
+#   *polygon_segment_3d(translated_v),
+#   Points3D(*dino_3d_vectors,color="red"),
+#   *polygon_segment_3d(dino_3d_vectors,color="red")
+# );
 
 
+print(list(zip(magic_matrix,(1,2,3))))
 # draw3d(*polygon_segment_3d(dino_3d_vectors));
 
-print(zip(*(1,3,4)))
+# print(zip(*(1,3,4)))
 
 
