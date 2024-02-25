73f8b2a0a3c472baf8b1bdfd2d704ee2c0b5e139
jxy001a <jxy001a@aliyun.com>
2024-02-25 19:34:00 +0800

    feat: play sound

--- chapter13/draw.py
+++ chapter13/draw.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 def plot_function(f,xmin,xmax,**kwargs):
   ts = np.linspace(xmin,xmax,1000)
   plt.plot(ts,[f(t) for t in ts],**kwargs)
---
+++ chapter13/play_sin.py
@@ -0,0 +1,35 @@
+from math import sin,pi
+from draw import *
+import pygame, pygame.sndarray
+from time import sleep
+
+
+
+pygame.mixer.init(frequency=44100, size=-16, channels=1)
+
+def make_sinusoid(frequency,amplitude):
+  def f(t):
+    return amplitude * sin(2 * pi *  frequency * t)
+  return f
+
+# plot_function(make_sinusoid(5,4),0,1)
+
+def simple(f,start,end,count):
+  mapf = np.vectorize(f)
+  
+  ts = np.arange(start,end,(end-start)/count)
+  
+  values = mapf(ts)
+  
+  return values.astype(np.int16)
+
+
+
+
+sinusoid = make_sinusoid(441,8000)
+arr = simple(sinusoid,0,1,44100)
+# print(arr)
+
+sound = pygame.sndarray.make_sound(arr)
+sound.play()
+sleep(1)
